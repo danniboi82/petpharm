aa2ba1ef81602388dc048cc5fad9580bd8d4d835
Unknown <danoh82@gmail.com>
2018-04-10 11:17:26 -0700

    basic page routing and attempt at category bar

--- client/src/Components/WebPages/Containers/ResponsiveContainers/DesktopContainer.jsx
+++ client/src/Components/WebPages/Containers/ResponsiveContainers/DesktopContainer.jsx
@@ -41,77 +41,72 @@ class DesktopContainer extends Component {
             vertical={true}
             className='NavbarSeg'
           >
-            <Container borderless>
-              <Menu
-                className='NavbarDiv'
-                fixed={fixed ? 'top' : null}
-                inverted={!fixed}
-                pointing={!fixed}
-                borderless
-                secondary={!fixed}
-                size='large'>
-                <Container>
-                  <Menu.Item>
-                    <h1>Pet Pharm</h1>
-                  </Menu.Item>
-                  <Search
-                    className="SearchBar"
-                  // loading={isLoading}
-                  // onResultSelect={this.handleResultSelect}
-                  // onSearchChange={_.debounce(this.handleSearchChange, 500, { leading: true })}
-                  // results={results}
-                  // value={value}
-                  // {...this.props}
-                  />
-                  <Menu.Item position='right'>
-                    <Menu.Item
-                      name='top'
-                      href='#Top'
-                      active={activeItem === 'top'}
-                      onClick={this.handleItemClick}>
-                      <Icon name='home' style={{ position: 'relative', bottom: '2px' }} />
-                      Home
+            <Menu
+              className='NavbarDiv'
+              fixed={fixed ? 'top' : null}
+              inverted={!fixed}
+              pointing={!fixed}
+              borderless
+              secondary={!fixed}
+              size='large'>
+              <Menu.Item>
+                <h1>Pet Pharm</h1>
+              </Menu.Item>
+              <Search
+                className="SearchBar"
+                // loading={isLoading}
+                // onResultSelect={this.handleResultSelect}
+                // onSearchChange={_.debounce(this.handleSearchChange, 500, { leading: true })}
+                // results={results}
+                // value={value}
+                // {...this.props}
+                input={{ fluid: true }}
+                aligned='left'
+              />
+              <Menu.Item position='right'>
+                <Menu.Item
+                  name='top'
+                  href='#Top'
+                  active={activeItem === 'top'}
+                  onClick={this.handleItemClick}>
+                  <Icon name='home' style={{ position: 'relative', bottom: '2px' }} />
+                  Home
                                     </Menu.Item>
-                    <Menu.Item
-                      href='#About'
-                      name='about'
-                      active={activeItem === 'about'}
-                      onClick={this.handleItemClick}>
-                      <Icon name='user outline' size='small' />
-                      About
+                <Menu.Item
+                  href='#About'
+                  name='about'
+                  active={activeItem === 'about'}
+                  onClick={this.handleItemClick}>
+                  <Icon name='user outline' size='small' />
+                  About
+                    </Menu.Item>
+                <Menu.Item
+                  href='#Products'
+                  name='products'
+                  active={activeItem === 'products'}
+                  onClick={this.handleItemClick}>
+                  <Icon name='code' size='small' />
+                  Products
                                     </Menu.Item>
-                    <Menu.Item
-                      href='#Products'
-                      name='products'
-                      active={activeItem === 'products'}
-                      onClick={this.handleItemClick}>
-                      <Icon name='code' size='small' />
-                      Products
+                <Menu.Item
+                  name='contact'
+                  href='#Contact'
+                  active={activeItem === 'contact'}
+                  onClick={this.handleItemClick}>
+                  <Icon name='mail outline' size='small' />
+                  Contact
                                     </Menu.Item>
-                    <Menu.Item
-                      name='contact'
-                      href='#Contact'
-                      active={activeItem === 'contact'}
-                      onClick={this.handleItemClick}>
-                      <Icon name='mail outline' size='small' />
-                      Contact
-                                    </Menu.Item>
-                  </Menu.Item>
-                </Container>
-              </Menu>
-            </Container>
+              </Menu.Item>
+            </Menu>
           </Segment>
-
           <CategoryBar
-            fixed={fixed ? 'top' : null}
-            inverted={!fixed}
-            pointing={!fixed}
-            borderless
-            secondary={!fixed}
-            clicked={this.handleItemClick} />
-
-
-
+          // fixed={fixed ? 'top' : null}
+          // inverted={!fixed}
+          // pointing={!fixed}
+          // borderless
+          // secondary={!fixed}
+          // clicked={this.handleItemClick} 
+          />
         </Visibility>
         {children}
         <Footer />
